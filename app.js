55235fa85c0a9e9ae0a2407514980a943064a8dc
KIMYOUNGWOON <youngwn2130@gmail.com>
2023-10-03 14:27:02 +0900

    프로젝트 추가 세팅

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const app = express();
-const PORT = 3000;
+const cors = require('cors');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+dotenv.config();
 
+const app = express();
+
+app.use(cors());
+app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Server is listening Port on ${PORT}`);
 });
