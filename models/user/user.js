6573586e9d977c84b489cd7f48eabae251b9e441
KIMYOUNGWOON <youngwn2130@gmail.com>
2023-10-04 11:33:38 +0900

    Feat: 회원가입 API

---
+++ models/user/user.js
@@ -0,0 +1,45 @@
+const { model, Schema } = require('mongoose');
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    agreement: {
+      type: Boolean,
+      default: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = model('User', userSchema);
+
+module.exports = User;
