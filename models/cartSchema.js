495c19d8510d7d8aa1f7eed37ad543b7f4dd327a
Jeonghye Lee <wjdwwidz@gmail.com>
2023-10-06 22:35:22 +0900

    Comment:Delete

--- models/cartSchema.js
+++ models/cartSchema.js
@@ -19,6 +19,5 @@ const CartSchema = new mongoose.Schema({
 );
 
 const Cart = mongoose.model('Cart', CartSchema);
-//mongoose가 모델 이름을 소문자로 변환하고 복수형으로 만들어 MongoDB컬렉션 이름으로 사용 
-module.exports = Cart; //Cart로 위에 적힌 스키마를 이용할 수 있다
+module.exports = Cart;
 
