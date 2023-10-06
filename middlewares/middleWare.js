01cd49a98428da599d25d810cd76eac38bb892d6
Jeonghye Lee <wjdwwidz@gmail.com>
2023-10-06 22:13:47 +0900

    Comment:Delete

--- middlewares/middleWare.js
+++ middlewares/middleWare.js
@@ -1,10 +1,3 @@
 
 const express = require('express');
 const app = express();
-
-// // const app = require('../app');
-
-// module.exports = (err, req, res, next) => {
-//   console.error(err); // 에러를 콘솔에 출력 또는 로깅할 수 있습니다.
-//   res.status(500).json({ error: 'Server Error' });
-// };
